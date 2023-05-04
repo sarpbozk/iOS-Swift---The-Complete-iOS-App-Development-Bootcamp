0b48d22d6ca1f1f3b9172f46746ca7e29c5a1a2f
Muhammet Bozkurt <mubozk@icloud.com>
2023-05-04 20:06:18 +0300

    CastingDemo

--- CastingDemo.playground/Contents.swift
+++ CastingDemo.playground/Contents.swift
@@ -8,7 +8,6 @@ class Animal {
     }
 }
 
-
 class Human: Animal {
     func code() {
         print("typing away")
@@ -20,3 +19,33 @@ class Fish: Animal {
         print("breathing underwater")
     }
 }
+
+let muhammet = Human(n: "Muhammet Bozkurt")
+let jack = Human(n: "Jack Bauer")
+let nemo = Fish(n: "Nemo")
+
+let neighbours = [muhammet, jack, nemo] // animal array
+
+func findNemo(from animals: [Animal]) {
+    for animal in animals {
+        if animal is Fish {
+            print(animal.name)
+            let fish = animal as! Fish
+            fish.breatheUnderWater()
+        }
+    }
+}
+
+findNemo(from: neighbours)
+
+//let neighbours1 = neighbours[0] // still animal
+//
+//let myDouble = 0.0
+//let doubleAsAnInt = Int(myDouble) // like this but not this // this is initializing
+//
+//if neighbours[0] is Human { // compares among animals
+//    print("First neighbour is human")
+//}
+//if jack is Human { // always true
+//    print("jack is human")
+//}
